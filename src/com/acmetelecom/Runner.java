db3265f19bf8c228bfff44ebc3d564c9397a0b42
douglas401 <douglaz.c@gmail.com>
2012-11-12 13:33:45 +0000

    initial import source codebase

---
+++ src/com/acmetelecom/Runner.java
@@ -0,0 +1,21 @@
+package com.acmetelecom;
+
+public class Runner {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Running...");
+        BillingSystem billingSystem = new BillingSystem();
+        billingSystem.callInitiated("447722113434", "447766511332");
+        sleepSeconds(20);
+        billingSystem.callCompleted("447722113434", "447766511332");
+        billingSystem.callInitiated("447722113434", "447711111111");
+        sleepSeconds(30);
+        billingSystem.callCompleted("447722113434", "447711111111");
+        billingSystem.callInitiated("447777765432", "447711111111");
+        sleepSeconds(60);
+        billingSystem.callCompleted("447777765432", "447711111111");
+        billingSystem.createCustomerBills();
+    }
+    private static void sleepSeconds(int n) throws InterruptedException {
+        Thread.sleep(n * 1000);
+    }
+}
