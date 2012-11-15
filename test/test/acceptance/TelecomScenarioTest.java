ca0d074300b49ff092d41f90fa87c95fbed4c3c4
douglas401 <douglaz.c@gmail.com>
2012-11-15 13:36:53 +0000

    Change context in scenario test

---
+++ test/test/acceptance/TelecomScenarioTest.java
@@ -0,0 +1,25 @@
+package test.acceptance;
+
+import org.junit.Test;
+
+public class TelecomScenarioTest {
+    TelecomTestContext telecom = new TelecomTestContext();
+
+    @Test
+    public void TestRunNoCalls() {
+
+    }
+
+    @Test
+    public void TestRunWithSingleCall() {
+        telecom.whileApplicationRuns()
+                .newCallAt(System.currentTimeMillis())
+                .from("447722113434")
+                .to("447766511332")
+                .forSeconds(20)
+                .afterGenerateBills();
+
+//                .assertPhoneNumber("447722113434").getsChargedBy(4.33)
+//                .verifyTotalCallUsageInMinutes(100);
+    }
+}
--- test/test/acceptance/TelecomTestContext.java
+++ test/test/acceptance/TelecomTestContext.java
@@ -1,41 +1,27 @@
 package test.acceptance;
 
-import java.util.List;
-
-import org.joda.time.DateTime;
-
-import com.acmetelecom.BillingSystem;
-import com.acmetelecom.IBillGenerator;
-import com.acmetelecom.customer.Customer;
-
 public class TelecomTestContext {
-	BillingSystem billSys; 
-	IBillGenerator billGen = new TestBillGenerator();
-	DateTime now;
-	
-	public TelecomTestContext(){
-		billSys = new BillingSystem(billGen);
+	TestBillingSystem billingSystem;
 
-	}
-	
-	public TelecomTestContext withCallStarts(String caller, String callee){
-		billSys.callInitiated(caller, callee);
-		return this;
+	public TelecomTestContext whileApplicationRuns(){
+        billingSystem = new TestBillingSystem();
+        return this;
 	}
 
-	public TelecomTestContext withCallEnds(String caller, String callee){
-		billSys.callCompleted(caller, callee);
-		return this;
+	public TestCallEvent newCallAt(long start){
+        return TestCallEvent.newCall(this, start);
 	}
 
-	public TelecomTestContext setCurrentTime(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public TelecomTestContext addCallRecord(TestCallEvent testCallEvent) {
+        this.billingSystem.addRecord(testCallEvent);
+        return this;
+    }
 
-	public TelecomTestContext letFewMinutesPast(int i) {
-		// TODO Auto-generated method stub
-		return this;
-	}
-	
+    public TelecomTestContext afterGenerateBills(){
+        // Calculation for bills
+
+        return this;
+    }
+
+
 }
